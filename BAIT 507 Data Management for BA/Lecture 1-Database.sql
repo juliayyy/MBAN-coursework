64e67cbff77836a2622c58e98b90acdfeb277729
julia <julia.yuxiao.yang@outlook.com>
2021-01-16 12:00:11 -0800

    Lecture 1 notes

---
+++ BAIT 507 Data Management for BA/Lecture 1-Database.sql
@@ -0,0 +1,14 @@
+-- What is database
+-- How to use database
+-- Why learn database
+-- Relational DBMS, Oracle
+
+-- What is Database?
+-- Data: raw facts concerning objects, people, and events, can be text, images, audio, or video
+-- Database: an organized collection of data, that are usually logically related
+-- Database Management Systems (DBMS): software system that enables users to define, create,
+--                                     maintain and control access to the database.
+
+-- Oracle and Oracle DBMS
+-- Introduction to Oracle Cloud: DB Users
+-- OS/Cloud Users vs. DB Users
